6523d8af1d013c81aef20157f25744abbdcda91b
Lai Dinh Thuan (Jack) <laidinhthuan123@gmail.com>
2023-06-20 16:18:13 +0700

    Enhance login file

--- src/auth/guard/auth-guard.tsx
+++ src/auth/guard/auth-guard.tsx
@@ -4,6 +4,7 @@ import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hook';
 //
 import { useAuthContext } from '../hooks';
+import axios from 'axios';
 
 // ----------------------------------------------------------------------
 
@@ -27,7 +28,7 @@ export default function AuthGuard({ children }: AuthGuardProps) {
 
   const [checked, setChecked] = useState(false);
 
-  const check = useCallback(() => {
+  const check = useCallback(async () => {
     if (!authenticated) {
       const searchParams = new URLSearchParams({ returnTo: window.location.href }).toString();
 
@@ -35,10 +36,33 @@ export default function AuthGuard({ children }: AuthGuardProps) {
 
       const href = `${loginPath}?${searchParams}`;
 
-      router.replace("");
+      router.replace('');
     } else {
-      router.push("dashboard")
-      setChecked(true);
+      const uid = sessionStorage.getItem('uid');
+      const token = sessionStorage.getItem('token');
+      if (uid) {
+        const config = {
+          headers: { Authorization: `Bearer ${token}` },
+        };
+        const url = `${process.env.NEXT_PUBLIC_BE_ADMIN_API}/api/v1/users/${uid}/roles`;
+        const resp = await axios.get(url, config);
+        if (resp.status === 200) {
+          setChecked(true);
+          sessionStorage.setItem('roleCode', resp.data.roleCode);
+          router.prefetch('dashboard');
+          router.prefetch('dashboard/user');
+          if (`${resp.data.roleCode}_`.includes('ACCOUNTANT1')) {
+            router.replace('dashboard');
+          } else {
+            router.replace('dashboard/user');
+          }
+        } else {
+          router.replace('');
+        }
+      } else {
+        router.replace('');
+      }
+
     }
   }, [authenticated, method, router]);
 
