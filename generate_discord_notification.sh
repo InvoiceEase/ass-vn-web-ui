4b4598b2c7a04b780f9ab7fc60d1de1c584f842f
minhtran-213 <minhtran21301@gmail.com>
2023-09-16 22:06:19 +0700

    Change discord role

--- generate_discord_notification.sh
+++ generate_discord_notification.sh
@@ -41,7 +41,7 @@ fi
 
 # Generate the discord_notification.json file
 echo '{
-  "content": "Deployment status for <@&1087630107537059880>",
+  "content": "Deployment status for <@&1013668275755954256>",
   "embeds": [
     {
       "title": "Deployment Status",
